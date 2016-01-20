eecddb821033ab44542ee5a2408689413e86252e
Lauren <lpien@comcast.net>
2016-01-20 12:39:40 -0500

    deleted extra tabs

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -26,7 +26,28 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.controller('TodoCtrl', function($scope) {
+.controller('TodoCtrl', function($scope, $ionicModal) {
+  /*$ioinicModal.fromTemplateURL('user-modal.html',{
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal){
+    $scope.modal = modal;
+  });
+  $scope.openModal = function(){
+    $scope.modal.show();
+  };
+  $scope.closeModal = function(){
+    $scope.modal.hide();
+  };
+  $scope.$on('$destroy', function(){
+    $scope.modal.remove();
+  });
+  $scope.$on('modal.hidden', function(){
+    //Execute action
+  });
+  $scope.$on('modal.removed', function(){
+    //Execute action
+  });*/
   $scope.tasks = [
                     {
                         "id":1,
