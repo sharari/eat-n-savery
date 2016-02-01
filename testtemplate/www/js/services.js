933c874808034fdd6f0dc4c94bb5c06ae35fc8d7
sharari <Sarah.Harari@rochester.edu>
2016-02-01 01:31:49 -0500

    fixed some links, messed around with the submit page

--- testtemplate/www/js/services.js
+++ testtemplate/www/js/services.js
@@ -1,19 +1,5 @@
 angular.module('starter.services', ['ngResource'])
 
 .factory('Session', function ($resource) {
-    var sessions = function () {
-        var deffered = $q.defer();
-        $http({
-          method: 'GET',
-          url: 'data.json'
-        }).success(function (data, status, headers, config) {
-          deffered.resolve(data);
-        }).error(function (data, status, headers, config) {
-          deffered.reject(status);
-        });
-
-        return deffered.promise;
-      };
-	return {
-		all: sessions,
+    return $resource('http://localhost:5000/sessions/:sessionId');
 });
