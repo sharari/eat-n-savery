43098b531db88bedcc97130ff8f55e7afcb09528
Lauren <lpien@comcast.net>
2016-03-25 23:01:46 -0400

    revert change

--- rubystuff/app/models/restaurant.rb
+++ rubystuff/app/models/restaurant.rb
@@ -1,8 +1,4 @@
 class Restaurant < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  #devise :database_authenticatable, :registerable,
-   #      :recoverable, :rememberable, :trackable, :validatable
 	has_many :users
 	has_many :submissions,through: :users
 end
