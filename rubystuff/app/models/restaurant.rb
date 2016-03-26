bec6aaadd4793176f5c3bb4f8b96651163f3aa40
Lauren <lpien@comcast.net>
2016-03-25 22:54:17 -0400

    server crash

--- rubystuff/app/models/restaurant.rb
+++ rubystuff/app/models/restaurant.rb
@@ -1,4 +1,8 @@
 class Restaurant < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  #devise :database_authenticatable, :registerable,
+   #      :recoverable, :rememberable, :trackable, :validatable
 	has_many :users
 	has_many :submissions,through: :users
 end
