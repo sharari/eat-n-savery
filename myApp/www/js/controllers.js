650a0cecd6de1acf0c1d7afb4533712e9fa51a06
Lauren <lpien@comcast.net>
2016-04-24 22:05:57 -0400

    google map in controllers.js crashes

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -257,6 +257,20 @@ angular.module('SimpleRESTIonic.controllers', [])
             vm.isCreating = false;
         }
 
+        /*$cordovaGeolocation.getCurrentPosition(options).then(function(position){
+ 
+            var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  }, function(error){
+    console.log("Could not get location");
+  });  */
+
         vm.objects = [];
         vm.edited = null;
         vm.isEditing = false;
